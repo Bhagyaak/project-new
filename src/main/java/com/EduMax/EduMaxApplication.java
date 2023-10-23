48b260058d7b81d7359324569fcf560ace9246e0
tharindu45 <prasadtharindu816@gmail.com>
2023-10-23 17:25:37 +0530

    Add startup project

---
+++ src/main/java/com/EduMax/EduMaxApplication.java
@@ -0,0 +1,13 @@
+package com.EduMax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduMaxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduMaxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/EduMax/EduMaxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EduMax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduMaxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
